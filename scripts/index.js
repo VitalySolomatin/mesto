272d6aaed7e794c16deca63759b8483ff95a0d0a
Vitaly Solomatin <r1k@yandex.ru>
2022-02-12 18:27:50 +0300

    исправления после 1 ревью

--- scripts/index.js
+++ scripts/index.js
@@ -2,45 +2,32 @@
 const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close');
 const popupOpenButtonElement = document.querySelector('.profile__button-edit');
-const popupCloseSaveElement = popupElement.querySelector('.popup__button-submit');
+const formElement = document.querySelector('.popup__container');
+const nameInput = formElement.querySelector('.popup__field_nameInput');
+const jobInput = formElement.querySelector('.popup__field_jobInput');
+const profileTitle = document.querySelector('.profile__title');
+const profileSubtitle = document.querySelector('.profile__subtitle');
+
 
 const openPopup = function() {
     popupElement.classList.add('popup_opened');
+    nameInput.value = profileTitle.textContent;
+    jobInput.value = profileSubtitle.textContent;
 };
 
 const closePopap = function() {
     popupElement.classList.remove('popup_opened');
 };
 
-const savePopap = function() {
-    popupElement.classList.remove('popup_opened');
-};
+//изменение полей profile--------------------------------------------------------------
+
+const formSubmitHandler = function (event) {
+    event.preventDefault();
+    profileTitle.textContent = nameInput.value;
+    profileSubtitle.textContent = jobInput.value;
+    closePopap();
+}
 
 popupOpenButtonElement.addEventListener('click', openPopup);
 popupCloseButtonElement.addEventListener('click', closePopap);
-popupCloseSaveElement.addEventListener('click', savePopap);
-//изменение полей profile--------------------------------------------------------------
-
-let formElement = document.querySelector('.popup__container');
-let nameInput = formElement.querySelector('input[name$="nameInput"]');
-let jobInput = formElement.querySelector('input[name$="jobInput"]');
-let profile__title = document.querySelector('.profile__title');
-let profile__subtitle = document.querySelector('.profile__subtitle');
-
-formElement.addEventListener('submit', function(event) {
-    event.preventDefault();
-    profile__title.textContent = nameInput.value;
-    profile__subtitle.textContent = jobInput.value;
-});
-
-
-//активнаый Like----------------------------------------------------------------------
-
-let likeElementByClass = document.querySelectorAll('.elements-change');
-
-for (let i=0, length = likeElementByClass.length; i < length; i++){
-    likeElementByClass[i].addEventListener('click', function() {
-           this.setAttribute('src', './images/like-active.svg')
-}
-    );
-}
+formElement.addEventListener('submit', formSubmitHandler);
